03a29d78384f1856db81be144f5e3b251706daa9
bsc24 <bchin08@gmail.com>
2021-01-28 20:18:59 -0500

    Initial commit p2?

---
+++ app/src/main/java/com/example/flickster/adapters/MovieAdapter.java
@@ -0,0 +1,59 @@
+package com.example.flickster.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.flickster.R;
+import com.example.flickster.models.Movie;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    Context context;
+    List<Movie> movies;
+
+    public MovieAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    // Usually involves inflating a layout from XML and returning the holder
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    // Involves populating the data into the item through holder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    // Returns total count of items in the list
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+        }
+    }
+}
